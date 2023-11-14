cdee2d80d877b8c259c5d5e702f43c93a9628a5d
Sharan <sharan.1908038@srec.ac.in>
2023-11-14 23:15:32 +0530

    Backend Code Template

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+// const cors = require("cors")
+// app.use(cors({
+// origin:"*",
+// }));
+
+
+// mongoose.connect('mongodb://0.0.0.0:27017/Sharan', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB:', error);
+//   });
+
+
+
+app.use(bodyParser.json());
+app.use(express.json())
+
+app.get("/hii", (req, res) => {
+    res.json({ userExists: true });
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
+  
+  
