012d7d0c07015604fba1b1f4d04a5985b85d97a3
Sharan <sharan.1908038@srec.ac.in>
2023-11-16 11:40:01 +0530

    Backend Push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-
+ 
 // const cors = require("cors")
 // app.use(cors({
 // origin:"*",
